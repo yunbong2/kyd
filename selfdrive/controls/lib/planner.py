22ac96125cdbc70317602fc3aa7b6c3eda169f8c
Vehicle Researcher <>
2020-10-13 19:00:43 +0900

    openpilot v0.7.9-release

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -14,8 +14,6 @@ from selfdrive.controls.lib.longcontrol import LongCtrlState, MIN_CAN_SPEED
 from selfdrive.controls.lib.fcw import FCWChecker
 from selfdrive.controls.lib.long_mpc import LongitudinalMpc
 from selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX
-from selfdrive.kegman_conf import kegman_conf
-
 
 MAX_SPEED = 255.0
 
@@ -86,8 +84,6 @@ class Planner():
     self.path_x = np.arange(192)
 
     self.params = Params()
-    self.kegman = kegman_conf()
-    self.mpc_frame = 0
     self.first_loop = True
 
   def choose_solution(self, v_cruise_setpoint, enabled):
@@ -131,30 +127,6 @@ class Planner():
 
     enabled = (long_control_state == LongCtrlState.pid) or (long_control_state == LongCtrlState.stopping)
     following = lead_1.status and lead_1.dRel < 45.0 and lead_1.vLeadK > v_ego and lead_1.aLeadK > 0.0
-
-    if self.mpc_frame % 1000 == 0:
-      self.kegman = kegman_conf()
-      self.mpc_frame = 0
-      
-    self.mpc_frame += 1
-    
-    if len(sm['model'].path.poly) and int(self.kegman.conf['slowOnCurves']):
-      path = list(sm['model'].path.poly)
-
-      # Curvature of polynomial https://en.wikipedia.org/wiki/Curvature#Curvature_of_the_graph_of_a_function
-      # y = a x^3 + b x^2 + c x + d, y' = 3 a x^2 + 2 b x + c, y'' = 6 a x + 2 b
-      # k = y'' / (1 + y'^2)^1.5
-      # TODO: compute max speed without using a list of points and without numpy
-      y_p = 3 * path[0] * self.path_x**2 + 2 * path[1] * self.path_x + path[2]
-      y_pp = 6 * path[0] * self.path_x + 2 * path[1]
-      curv = y_pp / (1. + y_p**2)**1.5
-
-      a_y_max = 2.975 - v_ego * 0.0375  # ~1.85 @ 75mph, ~2.6 @ 25mph
-      v_curvature = np.sqrt(a_y_max / np.clip(np.abs(curv), 1e-4, None))
-      model_speed = np.min(v_curvature)
-      model_speed = max(20.0 * CV.MPH_TO_MS, model_speed) # Don't slow down below 20mph
-    else:
-      model_speed = MAX_SPEED
 
     # Calculate speed for normal cruise control
     if enabled and not self.first_loop and not sm['carState'].gasPressed:
