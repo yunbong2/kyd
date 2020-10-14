0010799c5b1451ce3d9464992552de612ace1668
Vehicle Researcher <>
2020-10-14 15:19:58 +0900

    openpilot v0.7.9-release

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -10,7 +10,7 @@ const CanMsg HYUNDAI_TX_MSGS[] = {
   {832, 0, 8}, {832, 1, 8},  // LKAS11 Bus 0, 1
   {1265, 0, 4}, {1265, 1, 4}, {1265, 2, 4},  // CLU11 Bus 0, 1, 2
   {593, 2, 8},  // MDPS Bus 2
-  {1157, 0, 4},  // LFAHDA_MFC Bus 0
+  //{1157, 0, 4},  // LFAHDA_MFC Bus 0
   {1056, 0, 8},  // SCC11, Bus 0
   {1057, 0, 8},  // SCC12, Bus 0
   //{1290, 0, 8},  // SCC13, Bus 0
