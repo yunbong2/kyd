9340b1f80a341264fb16035766753c8c4279493c
Vehicle Researcher <>
2020-10-18 22:47:20 +0900

    openpilot v0.7.9-release

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -457,9 +457,9 @@ static void ui_draw_tpms(UIState *s) {
   char tpmsFr[32];
   char tpmsRl[32];
   char tpmsRr[32];
-  int viz_tpms_w = 220;
-  int viz_tpms_h = 120;
-  int viz_tpms_x = s->scene.viz_rect.x + 1300;
+  int viz_tpms_w = 230;
+  int viz_tpms_h = 140;
+  int viz_tpms_x = s->scene.viz_rect.x + 1290;
   int viz_tpms_y = s->scene.viz_rect.y + (bdr_s*1.5);
   float maxv = 0;
   float minv = 300;
@@ -501,38 +501,39 @@ static void ui_draw_tpms(UIState *s) {
   ui_draw_rect(s->vg, viz_tpms_x, viz_tpms_y, viz_tpms_w, viz_tpms_h, color, 20, 5);
 
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
+  ui_draw_text(s->vg, pos_x, 120, "타이어공기압", 48, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
   const int pos_x = viz_tpms_x + (viz_tpms_w / 2);
   snprintf(tpmsFl, sizeof(tpmsFl), "%.1f", s->scene.tpmsPressureFl);
   snprintf(tpmsFr, sizeof(tpmsFr), "%.1f", s->scene.tpmsPressureFr);
   snprintf(tpmsRl, sizeof(tpmsRl), "%.1f", s->scene.tpmsPressureRl);
   snprintf(tpmsRr, sizeof(tpmsRr), "%.1f", s->scene.tpmsPressureRr);
   if (s->scene.tpmsPressureFl < 34) {
-    ui_draw_text(s->vg, pos_x-50, 120, tpmsFl, 45, COLOR_RED, s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 140, tpmsFl, 42, COLOR_RED, s->font_sans_semibold);
   } else if (s->scene.tpmsPressureFl > 50) {
-    ui_draw_text(s->vg, pos_x-50, 120, "N/A", 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 140, "N/A", 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   } else {
-    ui_draw_text(s->vg, pos_x-50, 120, tpmsFl, 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 140, tpmsFl, 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   }
   if (s->scene.tpmsPressureFr < 34) {
-    ui_draw_text(s->vg, pos_x+50, 120, tpmsFr, 45, COLOR_RED, s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 140, tpmsFr, 42, COLOR_RED, s->font_sans_semibold);
   } else if (s->scene.tpmsPressureFr > 50) {
-    ui_draw_text(s->vg, pos_x+50, 120, "N/A", 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 140, "N/A", 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   } else {
-    ui_draw_text(s->vg, pos_x+50, 120, tpmsFr, 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 140, tpmsFr, 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   }
   if (s->scene.tpmsPressureRl < 34) {
-    ui_draw_text(s->vg, pos_x-50, 166, tpmsRl, 45, COLOR_RED, s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 170, tpmsRl, 42, COLOR_RED, s->font_sans_semibold);
   } else if (s->scene.tpmsPressureRl > 50) {
-    ui_draw_text(s->vg, pos_x-50, 166, "N/A", 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 170, "N/A", 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   } else {
-    ui_draw_text(s->vg, pos_x-50, 166, tpmsRl, 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x-50, 170, tpmsRl, 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   }
   if (s->scene.tpmsPressureRr < 34) {
-    ui_draw_text(s->vg, pos_x+50, 166, tpmsRr, 45, COLOR_RED, s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 170, tpmsRr, 42, COLOR_RED, s->font_sans_semibold);
   } else if (s->scene.tpmsPressureRr > 50) {
-    ui_draw_text(s->vg, pos_x+50, 166, "N/A", 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 170, "N/A", 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   } else {
-    ui_draw_text(s->vg, pos_x+50, 166, tpmsRr, 45, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
+    ui_draw_text(s->vg, pos_x+50, 170, tpmsRr, 42, COLOR_WHITE_ALPHA(200), s->font_sans_semibold);
   }
 }
 
