8fd6e040e6d3dca3f25a60681b8262b974cba2bd
Vehicle Researcher <>
2020-10-18 22:50:29 +0900

    openpilot v0.7.9-release

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -501,8 +501,8 @@ static void ui_draw_tpms(UIState *s) {
   ui_draw_rect(s->vg, viz_tpms_x, viz_tpms_y, viz_tpms_w, viz_tpms_h, color, 20, 5);
 
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
-  ui_draw_text(s->vg, pos_x, 120, "타이어공기압", 48, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
   const int pos_x = viz_tpms_x + (viz_tpms_w / 2);
+  ui_draw_text(s->vg, pos_x, 120, "타이어공기압", 48, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
   snprintf(tpmsFl, sizeof(tpmsFl), "%.1f", s->scene.tpmsPressureFl);
   snprintf(tpmsFr, sizeof(tpmsFr), "%.1f", s->scene.tpmsPressureFr);
   snprintf(tpmsRl, sizeof(tpmsRl), "%.1f", s->scene.tpmsPressureRl);
