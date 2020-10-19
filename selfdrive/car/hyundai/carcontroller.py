9eaef2dc2f3bd765c1bb45f1079d320dcfa336e0
Vehicle Researcher <>
2020-10-19 23:44:04 +0900

    openpilot v0.7.9-release

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -102,6 +102,7 @@ class CarController():
     self.yRel = 0
     self.vRel = 0
 
+    self.cruise_gap = 0
     self.cruise_gap_prev = 0
     self.cruise_gap_set_init = 0
     self.cruise_gap_switch_timer = 0
@@ -271,8 +272,10 @@ class CarController():
         self.lkas_switch = "ON"
       else:
         self.lkas_switch = "-"
+      self.cruise_gap = CS.cruiseGapSet
 
-      str_log2 = '주행모드={:s}  MDPS상태={:s}  LKAS버튼={:s}  CG:{:1.0f}'.format( self.steer_mode, self.mdps_status, self.lkas_switch, CS.cruiseGapSet )
+
+      str_log2 = '주행모드={:s}  MDPS상태={:s}  LKAS버튼={:s}  크루즈갭:{:1.0f}'.format( self.steer_mode, self.mdps_status, self.lkas_switch, self.cruise_gap )
       trace1.printf2( '{}'.format( str_log2 ) )
 
 
